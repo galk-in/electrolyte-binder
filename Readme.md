6c65c953ab54d27e3d12aa6355d5ce439ac3e450
Никита Галкин <n.galkin@smcs.com.ua>
2015-10-29 20:52:46 +0200

    Fix errors

--- Readme.md
+++ Readme.md
@@ -1,11 +1,11 @@
-#Electrolyte-binder
+# Electrolyte-binder
 
 This package custom loader for electrolyte package. It implements easy binding. You can bind your script, node module or value.
 
 Also you can overwrite electrolyte loading binds. It is usefull for testing.
 **Remember** in electrolyte, if two loaders know same entity, then electrolyte use last loader binding.
 
-##Example
+## Example
 ```javascript
 var IoC = require('electrolyte');
 var Binder = require('@galkin/electrolyte-binder');
@@ -13,7 +13,7 @@ var Binder = require('@galkin/electrolyte-binder');
 Binder.addScriptBinding('userRepository', './my_repository');
 Binder.addModuleBinding('mongo', 'mysql');
 Binder.addValueBinding('port', process.env.ENV_PORT || 3000);
-IoC.use(Binder.loader);
+IoC.use(Binder.makeLoader());
 
-IoC.create('port');
+console.log(IoC.create('port'));
 ```
--- index.js
+++ index.js
@@ -2,67 +2,72 @@ module.exports = new Binder();
 var path = require('path');
 var fs = require('fs');
 var existsSync = fs.existsSync || path.existsSync; // node <=0.6
-
 /**
  * @constructor
  */
 function Binder() {
-  this._binding = [];
+  this._bindings = [];
 }
 
 /**
- * @param id {String}
- * @returns {*}
+ * @returns Loader function for electrolyte
  */
-Binder.prototype.loader = function (id) {
-  var binding = this._binding[id];
-  if (!binding) return;
-  switch (binding.type) {
-    case 'script':
-      if (!existsSync(binding.value)) return;
-      return require(binding.value);
-      break;
-    case 'module':
-      try {
-        var module = require(id);
-        module['@literal'] = true;
-        return module;
-      } catch (e) {
-        return;
-      }
-      break;
-    case 'value':
-      return binding.value;
-      break;
-  }
-}
+Binder.prototype.makeLoader = function() {
+  var bindings = this._bindings;
+  return function(id) {
+    var binding = bindings[id];
+    if (!binding) return;
+    switch (binding.type) {
+      case 'script':
+        var script = path.resolve(binding.value);
+        if (!existsSync(script)) return;
+        return require(script);
+        break;
+      case 'module':
+        try {
+          var module = require(binding.value);
+          module['@literal'] = true;
+          return module;
+        } catch (e) {
+          return;
+        }
+
+        break;
+      case 'value':
+        return binding.value;
+        break;
+    }
+  };
+};
 
 /**
  * @param id {String}
  * @param path {String}
  */
-Binder.prototype.addScriptBinding = function (id, path) {
-  if ('string' == typeof id) throw Error('Expected id\'s type is string, but id is ' + id);
-  if ('string' == typeof path) throw Error('Expected path\'s type is string, but path is ' + path);
-  this._binding[id] = {type: 'script', value: path};
-}
+Binder.prototype.addScriptBinding = function(id, path) {
+  if (typeof id !== 'string') throw Error('Expected id\'s type is string, but id is ' + id);
+  if (typeof path !== 'string') throw Error('Expected path\'s type is string, but path is ' + path);
+  if (path.substr(path.length - 3) !== '.js'
+    &&  path.substr(path.length - 5) !== '.json') path = path + '.js';
+  this._bindings[id] = {type: 'script', value: path};
+};
 
 /**
  *
  * @param id {String}
  * @param moduleName {String}
  */
-Binder.prototype.addModuleBinding = function (id, moduleName) {
-  if ('string' == typeof id) throw Error('Expected id\'s type is string, but id is ' + id);
-  if ('string' == typeof moduleName) throw Error('Expected moduleName\'s type is string, but moduleName is ' + path);
-  this._binding[id] = {type: 'module', value: moduleName};
-}
+Binder.prototype.addModuleBinding = function(id, moduleName) {
+  if (typeof id !== 'string') throw Error('Expected id\'s type is string, but id is ' + id);
+  if (typeof moduleName !== 'string') throw Error('Expected moduleName\'s type is string, but moduleName is ' + path);
+  this._bindings[id] = {type: 'module', value: moduleName};
+};
 /**
  *
  * @param id {String}
  * @param value {*}
  */
-Binder.prototype.addValueBinding = function (id, value) {
-  if ('string' == typeof id) throw Error('Expected id\'s type is string, but id is ' + id);
-  this._binding[id] = {type: 'value', value: value};
-}
+Binder.prototype.addValueBinding = function(id, value) {
+  if (typeof id !== 'string') throw Error('Expected id\'s type is string, but id is ' + id);
+  this._bindings[id] = {type: 'value', value: value};
+};
